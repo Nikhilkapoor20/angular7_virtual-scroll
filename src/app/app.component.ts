ad66b6f2acdd8a9469dee3b7eab452557d92e078
Nikhil Kapoor <nikhil.kapoor@appinventiv.com>
2018-10-23 09:27:42 +0530

    virtual scrolling

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+}
